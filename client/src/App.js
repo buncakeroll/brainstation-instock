a1e9b2efc11081d69980ac2c7b26789a7bccfa18
Mariam Kilyana <mariamkilyana@Mariams-Air.localdomain>
2020-03-03 16:18:18 -0500

    Fixed route paths

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import warehouses from "./pages/Warehouses";
+import warehouses from "./pages/warehouses";
 import inventory from "./pages/inventory";
-import Header from "./components/Header";
+import Header from "./components/Header/Header";
 
 class App extends Component {
   render() {
