8bac45d119cd9517588b7875bfcbb2b299905a0d
Mariam Kilyana <mariamkilyana@Mariams-Air.localdomain>
2020-03-03 14:45:33 -0500

    Router and GET requests added

--- server/routes/inventory.js
+++ server/routes/inventory.js
@@ -1,6 +1,10 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const inventoryFile = require('../data/inventory.json');
+const inventoryList = require("../data/inventory.json");
 
+router.get("/", (_, res) => {
+  res.json(inventoryList);
+  res.status(200).send();
+});
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-const warehouseRoute = require('./routes/warehouse');
-const inventoryRoute = require('./routes/inventory');
-
+const warehouseRoute = require("./routes/warehouse");
+const inventoryRoute = require("./routes/inventory");
 
 const serverPort = process.env.PORT || 8080;
 
@@ -11,8 +10,8 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use('/inventory', inventoryRoute);
-app.use('/warehouse', warehouseRoute);
+app.use("/inventory", inventoryRoute);
+app.use("/warehouse", warehouseRoute);
 
 app.listen(serverPort, () => {
   console.log(`Listening on ${serverPort}`);
