562f8835f2d0ac16b013b6e81b4bc9262bf3ce83
Rafay Ali <rafayny@gmail.com>
2020-03-03 14:07:22 -0500

    matched the id to find both the warehouse and inventory associated with the warehouse. Pushed both data sets into one array and sent that back to client

--- server/routes/warehouse.js
+++ server/routes/warehouse.js
@@ -1,7 +1,29 @@
 const express = require('express');
 const router = express.Router();
 const warehouseList = require('../data/locations.json');
+const inventoryList = require('../data/inventory.json');
 
+router.get('/warehouse/:id', (req, res) => {
+    const warehouseData = warehouseList.find(item => {
+        return req.params.id === item.id;
+    });
+
+    const inventoryData = inventoryList.find(item => {
+        return req.params.id == item.warehouseId
+    });
+
+    if (warehouseData == undefined) {
+        res.status(404).send('Warehouse not found');
+    }
+
+    else {
+        const data = [];
+        data.push(warehouseData);
+        data.push(inventoryData);
+
+        res.json(data);
+    }
+})
 
 router.post('/', (req, res) => {
     let keys = Object.keys(req.body);
