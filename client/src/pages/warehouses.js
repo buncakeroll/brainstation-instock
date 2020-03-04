097a4168ff3c253c903943aa68cf5cb7e0d122d0
Rafay Ali <rafayny@gmail.com>
2020-03-04 04:18:58 -0500

    finished mobile layout without click functionality

--- client/src/pages/warehouses.js
+++ client/src/pages/warehouses.js
@@ -1,5 +1,19 @@
 import React from "react";
+import DisplayThing from './../components/DisplayPage/DisplayPage';
+import WarehouseCard from './../components/WarehouseCard/WarehouseCard';
+import './warehouses.scss';
 
 export default function Warehouses() {
-  return <div></div>;
+  return <div>
+    <DisplayThing
+      heading='Locations'
+    />
+    <div className='locations__labelscontainer'>
+      <p className='locations__label'>WAREHOUSE</p>
+      <p className='locations__label'>CONTACT</p>
+      <p className='locations__label'>CONTACT INFORMATION</p>
+      <p className='locations__label'>CATEGORIES</p>
+    </div>
+    <WarehouseCard />
+  </div>;
 }
