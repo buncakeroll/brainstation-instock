5b99e936975541bf522077d428f8420c342b5430
Anna <av.mastoris@gmail.com>
2020-03-04 14:08:32 -0500

    Incorporated inventory components into inventory page

--- client/src/pages/inventory.js
+++ client/src/pages/inventory.js
@@ -1,27 +1,17 @@
-<<<<<<< HEAD
-import React from "react";
-import InventoryTable from "../components/InventoryTable/InventoryTable";
-import DisplayPage from '../components/DisplayPage/DisplayPage';
-import AddButton from "../components/AddButton/AddButton";
-
-export default function Inventory() {
-    return (
-      <div>
-        <DisplayPage heading='Inventory'/>
-        <AddButton />
-        <InventoryTable />
-      </div>
-      );
-=======
 import React, { Component } from "react";
 import CreateProduct from '../components/AddComponent/CreateProduct';
+import InventoryTable from "../components/InventoryTable/InventoryTable";
+import DisplayPage from '../components/DisplayPage/DisplayPage';
 
 export default class Inventory extends Component {
     render() {
       return (
-        <CreateProduct />
+        <div>
+          <DisplayPage heading='Inventory'/>
+          <CreateProduct />
+          <InventoryTable />
+        </div>
       )
     }
->>>>>>> develop
   }
   
