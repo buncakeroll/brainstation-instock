51da7fd041e79900a4d4b23b94f964dfe77b6220
Millard <millard@millardle.com>
2020-03-04 15:29:58 -0500

    Show inventory table

--- client/src/pages/inventory.js
+++ client/src/pages/inventory.js
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { Component } from "react";
+import CreateProduct from '../components/AddComponent/CreateProduct';
+import InventoryTable from "../components/InventoryTable/InventoryTable";
+import DisplayPage from '../components/DisplayPage/DisplayPage';
 
-export default function Inventory() {
-    return <div></div>;
+export default class Inventory extends Component {
+    render() {
+      return (
+        <div>
+          <DisplayPage heading='Inventory'/>
+          <CreateProduct />
+          <InventoryTable />
+        </div>
+      )
+    }
   }
   
