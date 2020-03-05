561e492be80001f12ae54a7769078a7318bc4e90
Millard <millard@millardle.com>
2020-03-05 15:32:24 -0500

    removed eventdefault from createproduct.js and took out value in post req to readd to make lowercase

--- client/src/components/AddComponent/CreateProduct.js
+++ client/src/components/AddComponent/CreateProduct.js
@@ -23,7 +23,6 @@ export default class CreateProduct extends Component {
     };
 
     submitHandler = (event) => {
-        event.preventDefault();
         let productinput = event.target.product.value;
         let descriptioninput = event.target.description.value;
         let orderedinput = event.target.ordered.value;
